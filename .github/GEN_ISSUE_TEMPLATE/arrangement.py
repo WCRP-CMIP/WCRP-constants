fabd69689f4a859a81d4c3ee82eb505c85b88263
Dan Ellis <daniel.ellis.research@gmail.com>
2026-02-02 10:02:35 +0000

    Initial commit

---
+++ .github/GEN_ISSUE_TEMPLATE/arrangement.py
@@ -0,0 +1,13 @@
+# Arrangement Template Configuration
+
+TEMPLATE_CONFIG = {
+    'name': 'Add/Modify: Arrangement',
+    'description': 'Add or modify a grid arrangement in WCRP Universe',
+    'title': 'Add/Modify: Arrangement',
+    'labels': ['delta', 'arrangement', 'Review', 'keep-open'],
+    'issue_category': 'arrangement'
+}
+
+DATA = {
+    'issue_kind': ['New', 'Modify']
+}
---
+++ .github/GEN_ISSUE_TEMPLATE/resolution.py
@@ -0,0 +1,14 @@
+# Resolution Template Configuration
+
+TEMPLATE_CONFIG = {
+    'name': 'Add/Modify: Resolution',
+    'description': 'Add or modify a resolution in WCRP Universe',
+    'title': 'Add/Modify: Resolution',
+    'labels': ['delta', 'resolution', 'Review', 'keep-open'],
+    'issue_category': 'resolution'
+}
+
+# Data for this template
+DATA = {
+    'issue_kind': ['New', 'Modify']
+}
