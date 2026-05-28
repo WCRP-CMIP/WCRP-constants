a814be2b4033a228f0b177930c670a8019c44701
Daniel Ellis <daniel.ellis@ext.esa.int>
2026-05-28 07:43:49 +0100

    template update

--- .github/GEN_ISSUE_TEMPLATE/organisation.py
+++ .github/GEN_ISSUE_TEMPLATE/organisation.py
@@ -1,12 +1,4 @@
-# Organisation/Institution Template Configuration
-TEMPLATE_CONFIG = {
-    'name': 'Add/Modify: Organisation',
-    'description': 'Add or modify an organisation in WCRP Universe',
-    'title': 'Add/Modify: Organisation',
-    'labels': ['alpha', 'organisation', 'universe', 'Review', 'keep-open'],
-    'issue_category': 'organisation'
-}
-
+# Data for this template
 DATA = {
     'issue_kind': ['New', 'Modify']
 }
